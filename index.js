9ec652d456614e21784e7395d5a6e920fa062649
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-09-02 10:18:30 +0300

    add search function

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 const apiHost = " http://localhost:3000";
 
-const individualFare = 2000;
+const tripFare = 2000;
+
+
+
+
+//search ticket data
+// function createTicket(ticketId) {
+//     fetch(`${apiHost}/passenger/${ticketId}`)
+//     .then(response => {
+//         if(response.status != 200) {
+//             throw new Error(response.statusText);
+//         }
+//         else {
+//             return response.json();
+//         }
+//     })
+//     .then(ticket => {
+//         document.getElementById('departureTown').innerHTML = ticket.depTown;
+//         document.getElementById('arrivalTown').innerHTML = ticket.arrivalTown;
+//         document.getElementById('depDate').innerHTML = ticket.DepDate;
+//         document.getElementById('depTime').innerHTML = ticket.depTime;
+//         document.getElementById('newName').innerHTML = ticket.name;
+//         document.getElementById('newIdNumber').innerHTML = ticket.idNumber;
+//         document.getElementById('newNumOfSeats').innerHTML = ticket.numOfSeats;
+//         document.getElementById('newfareAmount').innerHTML = ticket.fareAmout.map(totalFare => (newfareAmount * triFare))
+//     })
+//     .catch(err => {
+//         console.log(err.message)
+//     })
+// }
+
+//submit form event
+// document.addEventListener('DOMContentLoaded', () => {
+//    form = document.querySelector('form')
+//    form.addEventListener('submit', evt => {
+//     evt.preventDefault();
+//     myTicket(evt.target.newName.value)
+//    })
+// })
+// function myTicket (ticketInput) {
+//     let li = document.createElement('li')
+//     li.textContent = `${ticketInput}`
+//     document.querySelector('#tripInputs').appendChild(li)
+// }
+
+
+
+
+//POST ticket to database
 
 function printTicket(name, idNumber, seats, totalFare) {
    const totalAmount = individualFare * seats
@@ -8,8 +56,11 @@ function printTicket(name, idNumber, seats, totalFare) {
     const data = {
         name: "Miss Shilah",
         idNumber: "1567392",
+        depTown: "Nairobi",
+        arrivalTown: "Eldoret",
+        date: "09/01/2022",
         seats: 1,
-        totalFare: `${totalAmount}`
+        totalFare: `${individualFare * seats}`
     };
 
     const ticketData = {
@@ -21,7 +72,7 @@ function printTicket(name, idNumber, seats, totalFare) {
         body: JSON.stringify(data),
     };
 
-    return fetch(`${apiHost}/passenger`, ticketData)
+    return fetch(`http://localhost:3000"/passenger`, ticketData)
     .then(response => response.json())
     .then(data => {
         document.body.innerHTML = object["id"];
@@ -32,7 +83,4 @@ function printTicket(name, idNumber, seats, totalFare) {
     });
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     printTicket();
-// })
 
